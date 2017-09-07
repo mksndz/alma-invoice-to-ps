25332fa79eb36d4658be01b346c373e8f369bb88
Mike Kanning <mikekanning@gmail.com>
2017-09-07 17:04:03 -0400

    add methods to AlmaXmlReader to extract vendor and chartstring signifiers

--- lib/objects/alma_xml_reader.rb
+++ lib/objects/alma_xml_reader.rb
@@ -1,9 +1,19 @@
 require 'nokogiri'
-
 class AlmaXmlReader
   INVOICE_NODE_NAME = 'invoice'.freeze
+  VENDOR_NODE_NAME = 'vendor_code'.freeze
+  CHARTSTRING_NODE_NAME = 'fund_type_desc'.freeze
   def self.invoice_nodes(file)
     data = Nokogiri File.read(file.path)
     data.css(INVOICE_NODE_NAME)
+  end
+  def self.vendor_from(invoice_node)
+    get_value VENDOR_NODE_NAME, invoice_node
+  end
+  def self.chartstring_from(invoice_node)
+    get_value CHARTSTRING_NODE_NAME, invoice_node
+  end
+  def self.get_value(node_name, node)
+    node.css(node_name).inner_text
   end
 end
