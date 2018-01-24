e538f3daeb19d2ef2690e8b0b6fcbadb57d178da
Mike Kanning <mikekanning@gmail.com>
2018-01-24 16:05:55 -0500

    skip over line items that have an amount of 0

--- lib/objects/line_item_factory.rb
+++ lib/objects/line_item_factory.rb
@@ -2,8 +2,11 @@ require_relative 'line_item'
 require_relative 'alma_xml_reader'
 class LineItemFactory
   def self.create_all_from(nodes, chartstring)
-    nodes.map do |node|
-      LineItem.new node, chartstring
+    line_items = []
+    nodes.each do |node|
+      line_item = LineItem.new node, chartstring
+      line_items << line_item if line_item.amount.to_f > 0.0
     end
+    line_items
   end
 end
