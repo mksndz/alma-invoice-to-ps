08d52a82d057b9f367a22472977f358b936d7a5e
Mike Kanning <mikekanning@gmail.com>
2018-07-06 16:18:50 -0400

    remove defunct test file

--- lib/tests/line_item_test.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/autorun'
-require_relative '../objects/invoice'
-require_relative '../objects/line_item'
-
-class LineItemTest < Minitest::Test
-  def setup
-    invoice = Invoice.new()
-  end
-end
